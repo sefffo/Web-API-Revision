bb0cbdad00e0a15e45a99874e115f13ab10653fa
Saif Lotfy <154231473+sefffo@users.noreply.github.com>
2026-04-02 20:25:46 +0200

    feat: add granular basket item endpoints (add, update quantity, remove)

--- ECommerce.Domain/Interfaces/IBasketRepository.cs
+++ ECommerce.Domain/Interfaces/IBasketRepository.cs
@@ -1,4 +1,4 @@
-﻿using ECommerce.Domain.Entities.BasketModule;
+using ECommerce.Domain.Entities.BasketModule;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,9 +8,12 @@ namespace ECommerce.Domain.Interfaces
     public interface IBasketRepository
     {
         Task<CustomerBasket?> GetBasketAsync(string id);
-
-        Task<CustomerBasket?> CreateOrUpdateCustomerBasketAsync(CustomerBasket customerBasket ,TimeSpan TTL=default);
-
+        Task<CustomerBasket?> CreateOrUpdateCustomerBasketAsync(CustomerBasket customerBasket, TimeSpan TTL = default);
         Task<bool> DeleteBasketAsync(string id);
+
+        // Granular item operations
+        Task<CustomerBasket?> AddItemAsync(string basketId, BasketItem item);
+        Task<CustomerBasket?> UpdateItemQuantityAsync(string basketId, int itemId, int quantity);
+        Task<CustomerBasket?> RemoveItemAsync(string basketId, int itemId);
     }
 }
